d7eb8510f650b950db8b5c7407825f34b76c68f7
AofPatiphan <patiphan.ma@hotmail.com>
2022-02-05 22:20:06 +0700

    save

--- client/src/config/env.jsx
+++ client/src/config/env.jsx
@@ -1,5 +1,6 @@
 const API_ENDPOINT_URL = 'http://localhost:5555';
-// const API_ENDPOINT_URL = 'https://8919-49-230-15-194.ngrok.io';
+// const API_ENDPOINT_URL =
+//     'https://b6ab-2403-6200-89a8-d58f-c96c-9a37-14d6-839d.ngrok.io';
 
 export { API_ENDPOINT_URL };
 
